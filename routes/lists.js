2edcf56b7ceccc1d73015c55b479b12a6ea551d8
Dylan Silva <rogdylan98@gmail.com>
2021-11-16 16:24:03 -0600

    working login

--- routes/lists.js
+++ routes/lists.js
@@ -12,4 +12,21 @@ router.get('/', requireAuth, asyncHandler(async (req,res) => {
   res.render('index', { title: 'Git It Done', lists });
 }));
 
+router.post('/', csrfProtection, requireAuth, asyncHandler(async (req,res) => {
+  const { userId } = req.session.auth;
+  const {
+    listName
+  } = req.body;
+  const newList = await db.List.create({
+    listName,
+    userId,
+  });
+  res.send(newList);
+  // console.log(newList);
+  // const lists = await db.List.findAll({
+  //   where: {userId: 1} // re-test when we have more lists
+  // });
+  // res.render('index', { title: 'Git It Done', lists, csrfToken: req.csrfToken(), });
+}));
+
 module.exports = router;
